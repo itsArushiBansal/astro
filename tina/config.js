0ce93fffdaaf5c4f4409a0dc721188d8d8cf1258
itsArushiBansal <bansalaarushi98@gmail.com>
2024-11-07 16:22:19 +0530

    tina

--- tina/config.js
+++ tina/config.js
@@ -9,8 +9,8 @@ const branch =
 
 export default defineConfig({
   branch,
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  token: process.env.TINA_TOKEN,
+  clientId: process.env.CLIENTID,
+  token: process.env.TOKEN,
 
   build: {
     outputFolder: "admin",
